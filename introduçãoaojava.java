a507f3c52adcbaa1329dde49983eb417e662a106
Mateus Martins Ferreira <mateusmferreira@live.com>
2017-03-23 17:39:17 -0300

    First commit

---
+++ introduçãoaojava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introdução.ao.java;
+import javax.swing.JOptionPane;
+
+public class introduçãoaojava {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite o seu nome: ");
+        
+        String senha = JOptionPane.showInputDialog("Digte sua senha: ");
+        
+        if(nome.equals("maria") && senha.equals("123456")){
+            JOptionPane.showMessageDialog(null, "Acesso Permitido");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Acesso Negado");
+        }
+    }
+    
+}
+
+
